7751f8ee5065552f0d16ab0dfa984a3d01f9dd8a
Hyndavi Koduru <hyshu14@gmail.com>
2026-01-01 11:34:15 +0530

    Change database connection password
    Updated the database connection password.

--- backend/db_connect.py
+++ backend/db_connect.py
@@ -4,12 +4,13 @@ try:
     conn = psycopg2.connect(
         dbname="tourism_project",
         user="capstone_user",
-        password="harikafinal01",
+        password="hyn1419",
         host="localhost",
         port="5432"
     )
     print(" Database connection successful!")
 except Exception as e:
     print(" Error connecting to database:", e)
+
 
 

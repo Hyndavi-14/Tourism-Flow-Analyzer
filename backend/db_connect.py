924d5659bef17362b6965bade3d69f2de1ffb055
Hyndavi Koduru <hyshu14@gmail.com>
2026-01-01 10:58:28 +0530

    Add files via upload

---
+++ backend/db_connect.py
@@ -0,0 +1,14 @@
+import psycopg2
+
+try:
+    conn = psycopg2.connect(
+        dbname="tourism_project",
+        user="capstone_user",
+        password="harikafinal01",
+        host="localhost",
+        port="5432"
+    )
+    print(" Database connection successful!")
+except Exception as e:
+    print(" Error connecting to database:", e)
+
